70af5b49d625e68adc24945bbf85b946a0ee8b8e
hari shankar <harishankar3618@gmail.com>
2025-01-10 00:50:47 +0530

    ff

--- Project/email_alert.py
+++ Project/email_alert.py
@@ -34,11 +34,17 @@ def check_and_store_receipt_email():
         print("Receipt email saved.")
     return receipt_email
 
-def send_mail(receipt_email,body):
+def send_mail_notfound(receipt_email):
     """Send an email alert for detected malware during the scan"""
     try:
         # Email subject and body
         subject = "Malware Scan Result"
+        body = """Dear User,
+
+The recent malware scan has been completed, and we are happy to inform you that no malware was found. Your system is safe and secure.
+
+Best regards,
+Your Security Team"""
         # Create the email components
         message = MIMEMultipart()
         message['From'] = sender_email
@@ -59,6 +65,40 @@ def send_mail(receipt_email,body):
     except Exception as e:
         print(f"Failed to send malware alert email: {e}")
 
+def send_mail_found(receipt_email,malware_details):
+    """Send an email alert for detected malware during the scan"""
+    try:
+        # Email subject and body
+        subject = "Malware Scan Result"
+        body = f"""Dear User,
+
+The recent malware scan has been completed. Unfortunately, malware has been detected on your system. Here are the details of the detected malware:
+
+{malware_details}
+
+Please take immediate action to address this issue and ensure your system is secure.
+
+Best regards,
+Team Ransomewatch"""
+        # Create the email components
+        message = MIMEMultipart()
+        message['From'] = sender_email
+        message['To'] = receipt_email
+        message['Subject'] = subject
+
+        # Attach the email body
+        message.attach(MIMEText(body, 'plain'))
+
+        # Connect to the Gmail SMTP server
+        with smtplib.SMTP('smtp.zoho.com', 465) as server:
+            server.starttls()  # Start TLS encryption
+            server.login(sender_email, sender_password)  # Log in to the server
+            server.sendmail(sender_email, receipt_email, message.as_string())  # Send the email
+
+        print("Check mail for results")
+
+    except Exception as e:
+        print(f"Failed to send malware alert email: {e}")
 
 
 
--- Project/scan_file.py
+++ Project/scan_file.py
@@ -2,7 +2,7 @@ import sys
 import os
 import logging
 from scanner import YaraScanner
-from email_alert import send_mail
+from email_alert import send_mail_notfound,send_mail_found
 
 # Configure logging
 logging.basicConfig(
@@ -29,28 +29,12 @@ def detect_malware(file_path, receipt_email, file_name):
                     print(f"  - {match}")
                     all_files_detected.append(f"{file_name}: {match}")
                 logging.info(f"Matches found in file: {file_name}")
-                ody = f"""Dear User,
-
-The recent malware scan has been completed. Unfortunately, malware has been detected on your system. Here are the details of the detected malware:
-
-{all_files_detected}
-
-Please take immediate action to address this issue and ensure your system is secure.
-
-Best regards,
-Team Ransomewatch"""
-                send_mail(receipt_email,body)
+                send_mail_found(receipt_email,all_files_detected)
                 logging.info(f"Malware alert email sent for file: {file_name}")
             else:
-                print(f"no matches found in file:{file_name}")
+                print(f"No matches found in file:{file_name}")
                 logging.info(f"No matches found in file: {file_name}")
-                body = """Dear User,
-
-The recent malware scan has been completed, and we are happy to inform you that no malware was found. Your system is safe and secure.
-
-Best regards,
-Your Security Team"""
-                send_mail(receipt_email,body)
+                send_mail_notfound(receipt_email)
         except FileNotFoundError as e:
             logging.error(f"File not found: {file_name}")
             print("File not found")
