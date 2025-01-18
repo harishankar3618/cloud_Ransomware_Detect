9b9f55c3ce44e05531287b8038ce173bcf8db17a
hari shankar <harishankar3618@gmail.com>
2025-01-18 14:29:20 +0530

    dd

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ use App\Http\Controllers\MalwareDetectionController;
 
 Route::get('/', [MalwareDetectionController::class, 'welcome'])->name('malware.detect');
 Route::post('/', [MalwareDetectionController::class, 'detectMalware']);
+Route::post('/ip-scan', [MalwareDetectionController::class, 'scanIp'])->name('ip.scan');
