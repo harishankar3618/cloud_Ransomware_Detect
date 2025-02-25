ee074cea966abe14ae1d381001e9b1a1ec4ad110
root <root@ransomware.hfppw2pkfl3uzhioqxd5o5qgoc.hx.internal.cloudapp.net>
2025-02-25 06:27:55 +0000

    error

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MalwareDetectionController;
 
+// Test route to ensure the basic functionality of the application
+Route::get('/test', function() {
+    return 'Test route is working!';
+});
 
 Route::get('/', [MalwareDetectionController::class, 'welcome'])->name('malware.detect');
 Route::post('/', [MalwareDetectionController::class, 'detectMalware']);
